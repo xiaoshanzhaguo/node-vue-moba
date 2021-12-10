8474a2364c9efa58ce33e3c6e8e695b10981701e
shanzhaguo <xiaoshanzhaguo@163.com>
2021-12-10 21:25:44 +0800

    创建分类【客户端】

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import router from './router'
 
 Vue.config.productionTip = false
 
+import http from './http'
+//挂载路由到原型对象
+Vue.prototype.$http = http
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
